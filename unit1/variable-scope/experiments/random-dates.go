63c5012c5eaf063766f70f6b39e79a885fefa637
Macbook <macbook@Daniel-MacBook-Pro.local>
2026-04-17 12:37:38 +0100

    added 'experiments' dir, and moved random-dates.go in

---
+++ unit1/variable-scope/experiments/random-dates.go
@@ -0,0 +1,44 @@
+// Random date refactored
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+var era = "AD"
+
+func main() {
+	days := 31
+	
+	for range(10) {
+		year := rand.Intn(4001) + 1000
+		month := rand.Intn(12) + 1
+
+		switch month {
+		case 2:
+			if year % 4 == 0 {
+				if year % 100 == 0 {
+					if year % 400 == 0 {
+						fmt.Printf("It's a leap year at 'division by 400'! Check: %5v \n", year) // redundant. just to satisfy verification
+						days = 29
+					} else {
+						days = 28
+					}
+				} else {
+					fmt.Printf("It's a leap year at 'not division by 100'! Check: %5v \n", year) // redundant. just to satisfy verification
+					days = 29
+				}
+			} else {
+				days = 28
+			}
+	
+		case 4, 6, 9, 11:
+			days = 30
+		}
+		
+		day := rand.Intn(days) + 1
+		fmt.Printf("era: %-5v year: %-5v month: %-5v day: %-5v \n", era, year, month, day)
+	}
+}
